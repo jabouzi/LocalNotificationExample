5e4295134f963bcd6d41ccafc630255139cda715
Skander Jabouzi <skander.jabouzi@geemedia.com>
2017-11-21 14:14:11 -0500

    add ios 8/9 notifications

--- LocalNotificationExample/LocalNotificationExample/ViewController.swift
+++ LocalNotificationExample/LocalNotificationExample/ViewController.swift
@@ -13,26 +13,31 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
         initNotificationSetupCheck()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func initNotificationSetupCheck() {
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert]) { (success, error) in
+      if #available(iOS 10.0, *) {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { (success, error) in
             if success {
                 print("success")
             } else {
                 print("error")
             }
         }
+      } else {
+        if(UIApplication.instancesRespond(to: #selector(UIApplication.registerUserNotificationSettings(_:)))) {
+          UIApplication.shared.registerUserNotificationSettings(UIUserNotificationSettings(types: [.alert, .badge, .sound], categories: nil))
+        }
+      }
     }
     
     @IBAction func createNotification(_ sender: UIButton) {
+      if #available(iOS 10.0, *) {
         let notification = UNMutableNotificationContent()
         notification.title = "Danger Will Robinson"
         notification.subtitle = "Something This Way Comes"
@@ -42,6 +47,16 @@ class ViewController: UIViewController {
         let request = UNNotificationRequest(identifier: "notification", content: notification, trigger: notificationTrigger)
         
         UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+      } else {
+        let notification = UILocalNotification()
+        notification.fireDate = NSDate(timeIntervalSinceNow: 5) as Date
+        notification.alertTitle = "Danger Will Robinson\nSomething This Way Comes"
+        notification.alertBody = "I need to tell you something, but first read this."
+        notification.timeZone = NSTimeZone.default
+        notification.applicationIconBadgeNumber = UIApplication.shared.applicationIconBadgeNumber + 1
+        
+        UIApplication.shared.scheduleLocalNotification(notification)
+      }
     }
 }
 
